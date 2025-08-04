d4ab923a1be034049bd3d86ee01425373586f4a1
Ayoub Akour <ayoubakour2004@gmail.com>
2025-08-04 17:47:53 +0100

    done

--- app/db/models/user.py
+++ app/db/models/user.py
@@ -1,23 +1,11 @@
 import uuid
 from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTableUUID
-from sqlalchemy import Column, String, Integer, Boolean
+from sqlalchemy import Column, String
 from app.db.base_class import Base
-'''
+
 class User(SQLAlchemyBaseUserTableUUID, Base):
     __tablename__ = "users"
     
     # Additional fields beyond the base user model
     first_name = Column(String, nullable=True)
     last_name = Column(String, nullable=True)
-    '''
-class User(SQLAlchemyBaseUserTableUUID,Base):
-    __tablename__ = "users"
-
-    id = Column(Integer, primary_key=True, index=True)
-    first_name = Column(String, nullable=False)
-    last_name = Column(String, nullable=False)
-    email = Column(String, unique=True, index=True, nullable=False)
-    hashed_password = Column(String, nullable=False)
-    is_active = Column(Boolean, default=True)
-    is_superuser = Column(Boolean, default=False)
-    is_verified = Column(Boolean, default=False)
